ef3336db0755ea1fd23deadd1af552d910b35b2a
Ruslan <ruslan-skribl1998@mail.ru>
2020-01-04 01:19:02 +0300

    Шаблоны и шаблонные тэги
    Можно разбить документ на блоки и использовать с помощью {%blockname %}
    Также описан синтаксис циклов/условий итп в django.

--- Django_project_course/Django_project_course/views.py
+++ Django_project_course/Django_project_course/views.py
@@ -3,5 +3,7 @@ from django.shortcuts import render
 def home_view(request):
     context = {
         'name': 'Ruslan Talypov',
+        'range': range(10),
+        
     }
     return render(request, 'home.html', context)
