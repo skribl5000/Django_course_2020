70e4efc9c105b6bb7d242ae90865a4e89be78567
Ruslan <ruslan-skribl1998@mail.ru>
2020-01-29 00:51:33 +0300

    Page iterration (pangination) added Info messages (create, edit, delete) added

--- Django_project_course/cities/views.py
+++ Django_project_course/cities/views.py
@@ -5,17 +5,18 @@ from django.views.generic.edit import FormView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .forms import CityForm
+from django.core.paginator import Paginator
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib import messages
 
 
 def home(request):
-    if request.method == 'POST':
-        form = CityForm(request.POST or None)
-        if form.is_valid(): 
-           print(form.cleaned_data)
-    form = CityForm()
     cities = City.objects.all()
     ids = City.objects.all().values('id')
-    return render(request,'cities/home.html',  {'object_list':cities, 'id_list': ids, 'form': form})
+    paginator = Paginator(cities, 4)
+    page = request.GET.get('page')
+    cities = paginator.get_page(page)
+    return render(request,'cities/home.html',  {'object_list':cities, 'id_list': ids, })
     # не забыть форму в контекст ->
 
 class CityDetailView(DetailView):
@@ -23,23 +24,27 @@ class CityDetailView(DetailView):
     context_object_name = 'object'
     template_name = 'cities/detail.html'
   
-class CityCreateView(CreateView):
+class CityCreateView(SuccessMessageMixin, CreateView):
     model = City
     form_class = CityForm
     template_name = "cities/create.html"
     success_url = reverse_lazy('city:home')
+    success_message = 'Город успешно создан'
 
-class CityUpdateView(UpdateView):
+class CityUpdateView(SuccessMessageMixin, UpdateView):
     model = City
     form_class = CityForm
     template_name = "cities/update.html"
     success_url = reverse_lazy('city:home')
+    success_message = 'Город успешно отредактирован'
 
-class CityDeleteView(DeleteView):
+class CityDeleteView(SuccessMessageMixin, DeleteView):
     model = City
     template_name = "cities/delete.html"
     success_url = reverse_lazy('city:home')
+    success_message = 'Город успешно удален'
 
     # Вариант удаления сразу
-    # def get(self, request, *args, **kwargs):
-    #     return self.post(request, *args, **kwargs)
+    def get(self, request, *args, **kwargs):
+        messages.success(request, 'Город успешно удален')
+        return self.post(request, *args, **kwargs)
